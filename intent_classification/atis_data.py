fixing issues in softmax

intent_classification/atis_data.py
@@ -60,8 +60,13 @@ class AtisData(object):
     padded_inputs = []
 
     for i in range(len(input)):
-      pad = [data_helper.PAD_ID] * (self.max_in_seq_len - len(input[i]))
-      padded_inputs.append(np.array(list(input[i] + pad), np.float32))
+      data = input[i]
+      if len(data) < self.max_in_seq_len:
+        pad = [data_helper.PAD_ID for _ in range(self.max_in_seq_len - len(data))]
+        data.extend(pad);
+      else:
+        data = data[:self.max_in_seq_len]
+      padded_inputs.append(np.array(data, np.float32))
 
     return np.array(padded_inputs)
 
@@ -74,13 +79,15 @@ class AtisData(object):
 
     return np.array(one_hot_encoded_labels)
 
+  def get_test_data(self):
+    return (self.get_padded_data(self.in_seq_test), self.get_one_hot_encoded_labels(self.labels_test))
 
   def get_next_batch(self, batch_size):
     train_input = []
     train_labels = []
     counter = 0
-    train_data_size = len(train_input)
-    last_index = self.batch_index+batch_size
+    train_data_size = len(self.in_seq_train)
+    last_index = self.batch_index + batch_size
     if last_index <= train_data_size:
       train_input = self.in_seq_train[self.batch_index : last_index]
       train_labels = self.labels_train[self.batch_index : last_index]

intent_classification/softmax_regression/train.py
@@ -18,10 +18,6 @@ def train():
 	
 	number_of_labels = atis.get_number_of_labels()
 	x_train, y_train = atis.get_next_batch(FLAGS.batch_size)
-	print(type(x_train))
-	print(type(y_train))
-	print(x_train[0])
-	print(y_train[0])
   	x = tf.placeholder(tf.float32, [None, FLAGS.max_in_seq_len])
 	W = tf.Variable(tf.zeros([FLAGS.max_in_seq_len, number_of_labels]))
 	b = tf.Variable(tf.zeros([number_of_labels]))
@@ -35,6 +31,11 @@ def train():
 	for _ in range(FLAGS.iterations):
 		batch_xs, batch_ys = atis.get_next_batch(FLAGS.batch_size)
 		sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+	correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+	test_x, test_y = atis.get_test_data()
+	print(sess.run(accuracy, feed_dict={x: test_x, y_: test_y}))
+
 
 
 def main(_):